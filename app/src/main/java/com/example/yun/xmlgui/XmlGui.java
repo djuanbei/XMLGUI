adaf13d56b7feccd7e87fb2f86db0590676a83f6
Liyun Dai <dlyun2009@gmail.com>
2018-10-23 22:29:51 +0800

    ok

--- app/src/main/java/com/example/yun/xmlgui/XmlGui.java
+++ app/src/main/java/com/example/yun/xmlgui/XmlGui.java
@@ -10,20 +10,20 @@ import android.widget.EditText;
 
 public class XmlGui extends AppCompatActivity {
     final String tag = XmlGui.class.getName();
-    /** Called when the activity is first created. */
+
+    /**
+     * Called when the activity is first created.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Button btnRunForm = (Button) this.findViewById(R.id.btnRunForm);
-        btnRunForm.setOnClickListener(new Button.OnClickListener()
-        {
-            public void onClick(View v)
-            {
+        btnRunForm.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
                 EditText formNumber = (EditText) findViewById(R.id.formNumber);
-                Log.i(tag,"Attempting to process Form #
-                                [" + formNumber.getText().toString() + "]");
-                        Intent newFormInfo = new Intent(XmlGui.this,RunForm.class);
+                Log.i(tag, "Attempting to process Form # [" + formNumber.getText().toString() + "]");
+                Intent newFormInfo = new Intent(XmlGui.this, RunForm.class);
                 newFormInfo.putExtra("formNumber",
                         formNumber.getText().toString());
                 startActivity(newFormInfo);
